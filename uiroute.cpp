a0df844aadc2be0906d18d5dc5825a470da5778a
Shambhavi Sudarsan <43706184+shambhavisudarsan@users.noreply.github.com>
2023-06-29 22:58:38 -0600

    Update uiroute.cpp
    Added algorithm 2 to 6

--- uiroute.cpp
+++ uiroute.cpp
@@ -100,12 +100,122 @@ std::vector<std::vector<int>> satelliteTable(std::string direction,int n,int m,s
                 }
             }
         }
-    }
+    }   
     return satellite_table;
 }
 
+void shrink(int node,std::string direction,int y,int gx,std::vector<std::vector<int>>&satellite_table){
+    if(node!=-1 && direction == "->"){
+        satellite_table[gx][node] = std::min(node,std::max(satellite_table[gx][node],y));
+    }
+    if(node!=-1 && direction == "<-"){
+        satellite_table[gx][node] = std::max(node,std::min(satellite_table[gx][node],y));
+    }
+}
+
+bool vanish(int vx,int vy, int ux,int uy, std::string direction,std::vector<std::vector<int>>&satellite_table,int n,int m,std::vector<std::vector<char>>&grid){
+    if(satellite_table[vx][vy] == vy){
+        return false;
+    }
+    if(horizontalBreaking(direction,ux,vy,n,m,grid)){
+        return false;
+    }
+    if(direction == "->" && ((vy-1>=1 && grid[ux][vy] == 'B' && grid[ux][vy-1] == 'U') || (vy == 0 && grid[ux][vy] == 'B'))){
+        return false;
+    }
+    if(direction == "<-" && ((vy+1<=m && grid[ux][vy] == 'B' && grid[ux][vy+1] == 'U') || (vy == m && grid[ux][vy] == 'B'))){
+        return false;
+    }
+    return true; 
+}
+
+bool cutoff(int vx,int vy, int ux,int uy, std::string direction,std::vector<std::vector<int>>&satellite_table,int n,int m,std::vector<std::vector<char>>&grid){
+    if(satellite_table[vx][vy] != vy){
+        return false;
+    }
+    if(!horizontalBreaking(direction,ux,vy,n,m,grid)){
+        return false;
+    }
+    if(direction == "->" && !((vy-1>=1 && grid[ux][vy] == 'B' && grid[ux][vy-1] == 'U') || (vy == 0 && grid[ux][vy] == 'B'))){
+        return false;
+    }
+    if(direction == "<-" && !((vy+1<=m && grid[ux][vy] == 'B' && grid[ux][vy+1] == 'U') || (vy == m && grid[ux][vy] == 'B'))){
+        return false;
+    }
+    return true; 
+}
+
+void block(int n,int m,int x,int y,std::vector<std::vector<char>>&grid,std::vector<std::vector<int>>&satellite_horizontal_left,std::vector<std::vector<int>>&satellite_horizontal_right,std::vector<std::vector<int>>&successor_horizontal_right,std::vector<std::vector<int>>&successor_horizontal_left){
+    satellite_horizontal_right[x][y] = y;
+    shrink(successor_horizontal_right[x][y],"->",y+1,x,satellite_horizontal_right);
+    successor_horizontal_right[x][y] = -1;
+    
+    satellite_horizontal_left[x][y] = y;
+    shrink(successor_horizontal_left[x][y],"<-",y-1,x,satellite_horizontal_left);
+    successor_horizontal_left[x][y] = -1;
+
+    int dx[4] = {1,1,-1,-1};
+    int dy[4] = {1,-1,1,-1};
+    for(int i=0;i<4;i++){
+        if(dy[i]<0){
+            if(vanish(x+dx[i],y,x+dx[i],y+dy[i],"->",satellite_horizontal_right,n,m,grid)){
+                std::cout<<"vanish<"<<'\n';
+                shrink(y,"->",y,x+dx[i],satellite_horizontal_right);
+            }
+            if(cutoff(x+dx[i],y+dy[i],x+dx[i],y,"<-",satellite_horizontal_left,n,m,grid)){
+                std::cout<<"cutoff<"<<'\n';
+                shrink(successor_horizontal_left[x+dx[i]][y],"<-",y+dy[i],x+dx[i],satellite_horizontal_left);
+            }
+        }
+        if(dy[i]>0){
+            if(vanish(x+dx[i],y,x+dx[i],y+dy[i],"<-",satellite_horizontal_left,n,m,grid)){
+                std::cout<<"vanish>"<<'\n';
+                shrink(y,"<-",y,x+dx[i],satellite_horizontal_left);
+            }
+            if(cutoff(x+dx[i],y+dy[i],x+dx[i],y,"->",satellite_horizontal_right,n,m,grid)){
+                std::cout<<"cutoff>"<<'\n';
+                shrink(successor_horizontal_right[x+dx[i]][y],"->",y+dy[i],x+dx[i],satellite_horizontal_right);
+            }
+        }
+    }
+}
+
+void print(int n,int m,std::vector<std::vector<int>>satellite_horizontal_right,std::vector<std::vector<int>>satellite_horizontal_left,std::vector<std::vector<int>>successor_horizontal_right,std::vector<std::vector<int>>successor_horizontal_left){
+    std::cout<<"\nSatellite Horizontal Left"<<'\n';
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            std::cout<<satellite_horizontal_left[i][j]<<" ";
+        }
+        std::cout<<'\n';
+    }
+    std::cout<<'\n';
+    std::cout<<"Satellite Horizontal Right"<<'\n';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m+1;j++){
+            std::cout<<satellite_horizontal_right[i][j]<<" ";
+        }
+        std::cout<<'\n';
+    }
+    std::cout<<'\n';
+    std::cout<<"Successor Horizontal Right"<<'\n';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            std::cout<<successor_horizontal_right[i][j]<<" ";
+        }
+        std::cout<<'\n';
+    }
+    std::cout<<'\n';
+    std::cout<<"Successor Horizontal Left"<<'\n';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            std::cout<<successor_horizontal_left[i][j]<<" ";
+        }
+        std::cout<<'\n';
+    }
+
+}
 int main(){
-    int t,n,m,x,y,b;
+    int t,n,m,x,y,b; 
 
     /* Number of Test Cases */
     std::cin>>t;
@@ -182,5 +292,38 @@ int main(){
         }
 
         // std::cout<<tableLookup(1,2,1,3,n,m,"->",satellite_horizontal_right,successor_horizontal_right,grid);
+
+        /*
+        Input Choice - A,C,Q,E (Add, Clear, Query, End)
+        */
+       char input;
+       std::cin>>input;
+        while(input!='E'){
+            if(input == 'Q'){
+                int sx,sy,tx,ty;
+                std::cin>>sx>>sy>>tx>>ty;
+                // std::vector<std::vector<int>> distance = bfs(n,m,grid,sx,sy,tx,ty);
+                // std::cout<<"Shortest Distance from ("<<sx<<","<<sy<<")"<<" is : "<<distance[tx][ty]<<"\n";
+                std::cin>>input;
+            }
+            else if(input == 'A'){
+                int x,y;
+                std::cin>>x>>y;
+                grid[x][y] = 'B';
+                block(n,m,x,y,grid,satellite_horizontal_left,satellite_horizontal_right,successor_horizontal_right,successor_horizontal_left);
+                print(n,m,satellite_horizontal_right,satellite_horizontal_left,successor_horizontal_right,successor_horizontal_left);
+                // grid[x][y]='B';
+                std::cin>>input;
+            }
+            else if(input == 'C'){
+                int x,y;
+                std::cin>>x>>y;
+                grid[x][y]='U';
+                std::cin>>input;
+            }
+            else{
+                break;
+            }
+        }
     }
 }
